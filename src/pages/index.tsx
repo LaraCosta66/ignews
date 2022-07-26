08285cad96a3c12ecec2cecbfa22e86b710bbdfd
LaraCosta66 <larasmila64@gmail.com>
2022-07-26 20:47:28 -0300

    Feat: Adding Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Head from "../../node_modules/next/head";
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ignews</title>
+        <title>Home | ignews</title>
       </Head>
-      <h1 >Hello world!!</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, Welcome </span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/Mulher.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
