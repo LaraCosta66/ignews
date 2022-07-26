4613803fd0dd140ade54c7f0c637937f06ee744b
LaraCosta66 <larasmila64@gmail.com>
2022-07-26 18:39:06 -0300

    feat:update branch

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "../../node_modules/next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ignews</title>
+      </Head>
+      <h1 >Hello world!!</h1>
+    </>
+  );
+}
