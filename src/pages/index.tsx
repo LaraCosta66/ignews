c14961d70b20e03c341ec08135119504a42594cc
LaraCosta66 <larasmila64@gmail.com>
2022-07-28 11:02:30 -0300

    feat: Adding Subscribe Button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "../../node_modules/next/head";
+import { SubscribeButton } from "../components/SubscribeButton/index";
 import styles from './home.module.scss'
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton/>
         </section>
         <img src="/images/Mulher.svg" alt="Girl coding" />
       </main>
