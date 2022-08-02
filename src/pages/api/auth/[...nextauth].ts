ba22bf9086ab170a2db8e204a5d749f4d3df4272
LaraCosta66 <larasmila64@gmail.com>
2022-08-02 11:45:56 -0300

    Feat: Saving users in Database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from '../../../../node_modules/faunadb/index'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -7,6 +9,22 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user',
-    }), 
+    }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
